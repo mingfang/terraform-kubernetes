2dca44e72fca706e3e609f38a69c666dfe6e8934
Ming Fang <mingfang@mac.com>
2017-09-20 23:38:03 -0400

    revert to use fan networking

--- kubernetes/knode/start.sh
+++ kubernetes/knode/start.sh
@@ -4,7 +4,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 export KMASTER="${kmaster}"
 
-cd ~root/docker-kubernetes-node && ./docker-setup.sh
+cd ~root/docker-kubernetes-node && ./fan-setup.sh
 
 until docker info; do
     echo "Waiting for Docker to start..."
