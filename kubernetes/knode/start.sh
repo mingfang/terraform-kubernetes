e3859735df5219883858510271e868d42bb895f9
Ming Fang <mingfang@mac.com>
2017-09-30 02:27:56 -0400

    add docker version label

--- kubernetes/knode/start.sh
+++ kubernetes/knode/start.sh
@@ -19,7 +19,8 @@ done
 AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
 INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
 AMI_ID=`curl http://169.254.169.254/latest/meta-data/ami-id`
-export LABELS="zone=${zone},aws.az=$AZ,aws.instance-type=$INSTANCE_TYPE,aws.ami-id=$AMI_ID"
+DOCKER=`docker version --format '{{.Server.Version}}'`
+export LABELS="zone=${zone},aws.az=$AZ,aws.instance-type=$INSTANCE_TYPE,aws.ami-id=$AMI_ID,docker=$DOCKER"
 echo "LABELS=$LABELS"
 
 cd ~root/docker-kubernetes-node
