4deb90f2d83274f964486e01f85480f3372c0379
Ming Fang <mingfang@mac.com>
2017-10-19 21:20:54 -0400

    add sha label

--- kubernetes/knode/start.sh
+++ kubernetes/knode/start.sh
@@ -3,24 +3,20 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 export KMASTER="${kmaster}"
+export ZONE="${zone}"
 
-cd ~root/docker-kubernetes-node && ./fan-setup.sh
+cd ~root/docker-kubernetes-node
+./fan-setup.sh
 
-until docker info; do
-    echo "Waiting for Docker to start..."
-    sleep 3
-done
-
-until curl -k https://$KMASTER:6443/healthz; do
-    echo "Waiting for kmaster to come online..."
-    sleep 10;
-done
+until docker info; do echo "Waiting for docker..."; sleep 10; done
+until curl https://$KMASTER:6443/healthz; do echo "Waiting for kmaster..."; sleep 10; done
 
 AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
 INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
 AMI_ID=`curl http://169.254.169.254/latest/meta-data/ami-id`
 DOCKER=`docker version --format '{{.Server.Version}}'`
-export LABELS="zone=${zone},aws.az=$AZ,aws.instance-type=$INSTANCE_TYPE,aws.ami-id=$AMI_ID,docker=$DOCKER"
+SHA=$(git rev-parse --short HEAD)
+export LABELS="zone=$ZONE,sha=$SHA,az=$AZ,type=$INSTANCE_TYPE,ami=$AMI_ID,docker=$DOCKER"
 echo "LABELS=$LABELS"
 
 export VAULT_ADDR=http://$KMASTER:8200
