bdce7354bce59d5358800763ad51fd6ea738519d
Ming Fang <mingfang@mac.com>
2017-11-05 11:49:54 -0500

    add kubernetes failure domain labels

--- kubernetes/knode/start.sh
+++ kubernetes/knode/start.sh
@@ -5,23 +5,35 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 export KMASTER="${kmaster}"
 export ZONE="${zone}"
 
+#setup fan networking
+
 cd ~root/docker-kubernetes-node
 ./fan-setup.sh
+
+#wait for dependencies
 
 until docker info; do echo "Waiting for docker..."; sleep 10; done
 until curl -k https://$KMASTER:6443/healthz; do echo "Waiting for kmaster..."; sleep 10; done
 
-AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
-INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
-AMI_ID=`curl http://169.254.169.254/latest/meta-data/ami-id`
-DOCKER=`docker version --format '{{.Server.Version}}'`
+#setup labels
+
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -c -r .region)
+AZ=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
+INSTANCE_TYPE=$(curl http://169.254.169.254/latest/meta-data/instance-type)
+AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id)
+DOCKER=$(docker version --format '{{.Server.Version}}')
 SHA=$(git rev-parse --short HEAD)
-export LABELS="zone=$ZONE,sha=$SHA,az=$AZ,type=$INSTANCE_TYPE,ami=$AMI_ID,docker=$DOCKER"
+export LABELS="zone=$ZONE,sha=$SHA,failure-domain.beta.kubernetes.
+io/region=$REGION,failure-domain.beta.kubernetes.io/zone=$AZ,beta.kubernetes.io/instance-type=$INSTANCE_TYPE,ami=$AMI_ID,docker=$DOCKER"
 echo "LABELS=$LABELS"
+
+#setup vault
 
 export VAULT_ADDR=http://$KMASTER:8200
 AUTH_RESULT=$(curl -X POST $VAULT_ADDR/v1/auth/aws/login -d '{"role": "knode", "pkcs7":"'"$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d \\n)"'"}')
 export VAULT_TOKEN=$(echo $AUTH_RESULT | jq -r .auth.client_token)
+
+#start
 
 cd ~root/docker-kubernetes-node
 ./run $KMASTER
