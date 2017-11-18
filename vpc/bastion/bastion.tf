ddc1f257dbe69d833ab226637a91a78d61e6cc7c
Ming Fang <mingfang@mac.com>
2017-11-18 15:46:48 -0500

    limit bastion egress to port 22

--- vpc/bastion/bastion.tf
+++ vpc/bastion/bastion.tf
@@ -30,15 +30,15 @@ resource "aws_security_group" "bastion" {
 
   ingress {
     protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     protocol    = -1
-    from_port   = 0
-    to_port     = 0
+    from_port   = 22
+    to_port     = 22
     cidr_blocks = ["${var.vpc_cidr}"]
   }
 
