6e9162d241c374d0a58702b3a2b01ead18f22086
Ming Fang <mingfang@mac.com>
2020-02-24 20:37:57 -0500

    vault - auto create kubelet and proxy tokens

--- kubernetes/knode/start.sh
+++ kubernetes/knode/start.sh
@@ -25,16 +25,23 @@ AMI_ID=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
 DOCKER=$(docker version --format '{{.Server.Version}}')
 SHA=$(git rev-parse --short HEAD)
 export LABELS="zone=$ZONE,sha=$SHA,ami=$AMI_ID,docker=$DOCKER,instance-id=$INSTANCE_ID"
-export LABELS="$LABELS,kubernetes.io/role=$ZONE,failure-domain.beta.kubernetes.io/region=$REGION,failure-domain.beta.kubernetes.io/zone=$AZ,beta.kubernetes.io/instance-type=$INSTANCE_TYPE"
+export LABELS="$LABELS,failure-domain.beta.kubernetes.io/region=$REGION,failure-domain.beta.kubernetes.io/zone=$AZ,beta.kubernetes.io/instance-type=$INSTANCE_TYPE"
 echo "LABELS=$LABELS"
 
 #setup vault
 
 export VAULT_ADDR=http://$KMASTER:8200
-AUTH_RESULT=$(curl -X POST $VAULT_ADDR/v1/auth/aws/login -d '{"role": "knode", "pkcs7":"'"$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d \\n)"'"}')
-export VAULT_TOKEN=$(echo $AUTH_RESULT | jq -r .auth.client_token)
+AUTH_RESULT=$(curl -s -X POST $VAULT_ADDR/v1/auth/aws/login -d '{"role": "knode", "pkcs7":"'"$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/pkcs7 | tr -d \\n)"'"}')
+VAULT_TOKEN=$(echo $AUTH_RESULT | jq -r .auth.client_token)
 
-/attach_volume.py --tag Zone --value "$ZONE" --attach_as /dev/xvdf || true
+KUBELET_TOKEN_CURL=$(curl -s -X POST -H "X-Vault-Token: $VAULT_TOKEN" -H "X-Vault-Wrap-Ttl: 1m0s" -d '{"ttl":"1m0s","explicit_max_ttl":"0s","period":"0s","no_parent":true,"display_name":"","num_uses":0,"renewable":true,"type":"service"}' $VAULT_ADDR/v1/auth/token/create/kubelet)
+export KUBELET_TOKEN=$(echo $KUBELET_TOKEN_CURL | jq -r .wrap_info.token)
+
+PROXY_TOKEN_CURL=$(curl -s -X POST -H "X-Vault-Token: $VAULT_TOKEN" -H "X-Vault-Wrap-Ttl: 1m0s" -d '{"ttl":"1m0s","explicit_max_ttl":"0s","period":"0s","no_parent":true,"display_name":"","num_uses":0,"renewable":true,"type":"service"}' $VAULT_ADDR/v1/auth/token/create/proxy)
+export PROXY_TOKEN=$(echo $PROXY_TOKEN_CURL | jq -r .wrap_info.token)
+
+# auto attach volumes
+#/attach_volume.py --tag Zone --value "$ZONE" --attach_as /dev/xvdf || true
 
 #start
 cd ~root/docker-kubernetes-node
