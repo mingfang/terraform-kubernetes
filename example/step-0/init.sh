9ec744c00d8e5c0206d31d8136b7a7d69cc6eb9c
Ming Fang <mingfang@mac.com>
2021-12-20 03:34:23 +0000

    add script to initialize backend

---
+++ example/step-0/init.sh
@@ -0,0 +1,11 @@
+# Create S3 bucket and DynamoDB locking table for Terraform S3 Backend
+
+# initialize backend
+
+terraform init
+terraform apply
+
+# save state to backend
+
+terraform init -force-copy
+rm *.tfstate*
