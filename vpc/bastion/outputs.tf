068a440ca818a442b41a8e7d99e6881adf38992a
Ming Fang <mingfang@mac.com>
2022-10-02 01:59:23 +0000

    output private ip

--- vpc/bastion/outputs.tf
+++ vpc/bastion/outputs.tf
@@ -1,3 +1,7 @@
 output "fqdn" {
   value = var.enable && length(aws_route53_record.bastion) > 0 ? aws_route53_record.bastion.0.fqdn : ""
 }
+
+output "private_ip" {
+  value = var.enable ? aws_instance.bastion.0.private_ip : ""
+}
